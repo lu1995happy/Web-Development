ed32820a73e2108809d8b990e9ef0252d950da54
Tianlin Lu <txl355@case.edu>
2018-12-17 02:40:03 -0800

    adding register function and profile update

--- face_recognition_brain/src/components/SignIn/SignIn.js
+++ face_recognition_brain/src/components/SignIn/SignIn.js
@@ -4,7 +4,7 @@ class SignIn extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      signInEmail: ''.
+      signInEmail: '',
       signInPassword: ''
     }
   }
@@ -27,8 +27,9 @@ class SignIn extends React.Component {
       })
     })
       .then(response => response.json())
-      .then(data => {
-        if (data === 'success') {
+      .then(user => {
+        if (user.id) {
+          this.props.loadUser(user);
           this.props.onRouteChange('home');
         }
       })
--- face_recognition_brain/src/server.js
+++ face_recognition_brain/src/server.js
@@ -29,26 +29,25 @@ const database = {
   ]
 }
 
-app.get('./', (req, res) => {
+app.get('/', (req, res) => {
   res.send(database.users);
 })
 
 app.post('/signin', (req, res) => {
   if (req.body.email === database.users[0].email &&
       req.body.password === database.users[0].password) {
-    res.json('success');
+    res.json(database.users[0]);
   } else {
     res.status(400).json('error logging in');
   }
 })
 
-app.post('./register', (req, res) => {
+app.post('/register', (req, res) => {
   const {email, name, password} = req.body;
   database.users.push({
     id: '125',
     name: name,
     email: email,
-    password: password,
     entries: 0,
     joined: new Date()
   })
@@ -69,7 +68,7 @@ app.get('/profile/:id', (req, res) => {
   }
 })
 
-app.put('./image', (req, res) => {
+app.put('/image', (req, res) => {
   const { id } = req.body;
   let found = false;
   database.users.forEach(user => {
